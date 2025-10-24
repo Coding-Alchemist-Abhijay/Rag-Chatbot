fe36b75e25f3a85409c9194cbcbb59b2025aadf4
Abhijay Singh <singhabhijay540@gmail.com>
2025-10-24 21:29:44 +0530

    bug-fix-2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import path from "path";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  webpack: (config: any) => { // eslint-disable-line @typescript-eslint/no-explicit-any
+    config.resolve = config.resolve || {};
+    config.resolve.alias = config.resolve.alias || {};
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
 };
 
 export default nextConfig;
